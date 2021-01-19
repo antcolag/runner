5b294aa53de60d368f1f07089171718204298a79
anto <antcolag@gmail.com>
2021-01-19 22:55:30 +0100

    namespace cmd

--- src/ArduinoCmd.hpp
+++ src/ArduinoCmd.hpp
@@ -1,93 +1,95 @@
 #include <runner.hpp>
 
 namespace runner {
-	struct PinModeCmd : Command {
+	namespace cmd {
+		struct PinModeCmd : Command {
 
-		RUNNER_COMMAND(PinModeCmd)
+			RUNNER_COMMAND(PinModeCmd)
 
-		int8_t run(
-			InterfaceBase *,
-			String args[],
-			Stream &,
-			Stream & o,
-			Stream &
-		){
-			auto s = StringStream(args + 1);
-			int a = s.parseInt(), b = s.parseInt();
-			pinMode(a, b);
-			return 0;
-		}
-	};
+			int8_t run(
+				InterfaceBase *,
+				String args[],
+				Stream &,
+				Stream & o,
+				Stream &
+			){
+				auto s = StringStream(args + 1);
+				int a = s.parseInt(), b = s.parseInt();
+				pinMode(a, b);
+				return 0;
+			}
+		};
 
-	struct DigitalReadCmd : Command {
+		struct DigitalReadCmd : Command {
 
-		RUNNER_COMMAND(DigitalReadCmd)
+			RUNNER_COMMAND(DigitalReadCmd)
 
-		int8_t run(
-			InterfaceBase *,
-			String args[],
-			Stream &,
-			Stream & o,
-			Stream &
-		){
-			auto s = StringStream(args + 1);
-			int a = s.parseInt(), b = s.parseInt();
-			o.println(digitalRead(a));
-			return 0;
-		}
-	};
+			int8_t run(
+				InterfaceBase *,
+				String args[],
+				Stream &,
+				Stream & o,
+				Stream &
+			){
+				auto s = StringStream(args + 1);
+				int a = s.parseInt(), b = s.parseInt();
+				o.println(digitalRead(a));
+				return 0;
+			}
+		};
 
-	struct DigitalWriteCmd : Command {
+		struct DigitalWriteCmd : Command {
 
-		RUNNER_COMMAND(DigitalWriteCmd)
+			RUNNER_COMMAND(DigitalWriteCmd)
 
-		int8_t run(
-			InterfaceBase *,
-			String args[],
-			Stream &,
-			Stream & o,
-			Stream &
-		){
-			auto s = StringStream(args + 1);
-			int a = s.parseInt(), b = s.parseInt();
-			digitalWrite(a, b);
-			return 0;
-		}
-	};
+			int8_t run(
+				InterfaceBase *,
+				String args[],
+				Stream &,
+				Stream & o,
+				Stream &
+			){
+				auto s = StringStream(args + 1);
+				int a = s.parseInt(), b = s.parseInt();
+				digitalWrite(a, b);
+				return 0;
+			}
+		};
 
-	struct AnalogReadCmd : Command {
+		struct AnalogReadCmd : Command {
 
-		RUNNER_COMMAND(AnalogReadCmd)
+			RUNNER_COMMAND(AnalogReadCmd)
 
-		int8_t run(
-			InterfaceBase *,
-			String args[],
-			Stream &,
-			Stream & o,
-			Stream &
-		){
-			auto s = StringStream(args + 1);
-			int a = s.parseInt();
-			o.println(analogRead(a));
-			return 0;
-		}
-	};
+			int8_t run(
+				InterfaceBase *,
+				String args[],
+				Stream &,
+				Stream & o,
+				Stream &
+			){
+				auto s = StringStream(args + 1);
+				int a = s.parseInt();
+				o.println(analogRead(a));
+				return 0;
+			}
+		};
 
-	struct AnalogWriteCmd : Command {
+		struct AnalogWriteCmd : Command {
 
-		RUNNER_COMMAND(AnalogWriteCmd)
+			RUNNER_COMMAND(AnalogWriteCmd)
 
-		int8_t run(
-			InterfaceBase *,
-			String args[],
-			Stream &,
-			Stream & o,
-			Stream &
-		){
-			auto s = StringStream(args + 1);
-			int a = s.parseInt(), b = s.parseInt();
-			analogWrite(a, b);
-			return 0;
-		}
-	};
+			int8_t run(
+				InterfaceBase *,
+				String args[],
+				Stream &,
+				Stream & o,
+				Stream &
+			){
+				auto s = StringStream(args + 1);
+				int a = s.parseInt(), b = s.parseInt();
+				analogWrite(a, b);
+				return 0;
+			}
+		};
+	}
 }
--- src/utilsCmd.hpp
+++ src/utilsCmd.hpp
@@ -3,185 +3,189 @@
 
 namespace runner
 {
-	struct StreamDump : Command {
+	namespace cmd {
 
-		RUNNER_COMMAND(StreamDump)
 
-		int8_t run(
-			InterfaceBase * scope,
-			String args[],
-			Stream & input,
-			Stream & out,
-			Stream & err
-		){
-			Stream * in = &input;
-			if(args[1].length()) {
-				const auto link = scope->find<Stream>(args[1]);
-				if(!link){
-					err.println("argument not valid");
+		struct StreamDump : Command {
+
+			RUNNER_COMMAND(StreamDump)
+
+			int8_t run(
+				InterfaceBase * scope,
+				String args[],
+				Stream & input,
+				Stream & out,
+				Stream & err
+			){
+				Stream * in = &input;
+				if(args[1].length()) {
+					const auto link = scope->find<Stream>(args[1]);
+					if(!link){
+						err.println("argument not valid");
+						return 1;
+					}
+					in = link->ref();
+				}
+
+				if(!in || in == &NullStream::dev){
+					err.println("input not valid");
 					return 1;
 				}
-				in = link->ref();
-			}
 
-			if(!in || in == &NullStream::dev){
-				err.println("input not valid");
-				return 1;
-			}
-
-			int current = 0;
-			while(in->available()){
-				char idx[9];
-				sprintf(idx, "%6d: ", current);
-				current+=8;
-				out.print(idx);
-				uint8_t data[9];
-				data[8] = '\0';
-				for(int i = 0; i < 8; i++){
-					if(!in->available()){
-						goto endloop;
+				int current = 0;
+				while(in->available()){
+					char idx[9];
+					sprintf(idx, "%6d: ", current);
+					current+=8;
+					out.print(idx);
+					uint8_t data[9];
+					data[8] = '\0';
+					for(int i = 0; i < 8; i++){
+						if(!in->available()){
+							goto endloop;
+						}
+						char buff[5];
+						data[i] = in->read();
+						sprintf(buff, "%2x  ", (int) data[i]);
+						out.print(buff);
 					}
-					char buff[5];
-					data[i] = in->read();
-					sprintf(buff, "%2x  ", (int) data[i]);
-					out.print(buff);
+					out.println((char *) data);
 				}
-				out.println((char *) data);
+				if(!current){
+					err.println("input not available");
+				}
+				endloop:
+				return 0;
 			}
-			if(!current){
-				err.println("input not available");
+		};
+
+		struct FreeMemory : Command {
+
+			RUNNER_COMMAND(FreeMemory)
+
+			int8_t run(
+				InterfaceBase * scope,
+				String args[],
+				Stream & input,
+				Stream & out,
+				Stream &
+			){
+				int v = (int) ((int) &v - (int) (__brkval ?: &__heap_start));
+				out.print(v);
+				out.println('B');
+				return v > 0;
 			}
-			endloop:
-			return 0;
-		}
-	};
+		};
 
-	struct FreeMemory : Command {
+		struct Echo : Command {
 
-		RUNNER_COMMAND(FreeMemory)
+			RUNNER_COMMAND(Echo)
 
-		int8_t run(
-			InterfaceBase * scope,
-			String args[],
-			Stream & input,
-			Stream & out,
-			Stream &
-		){
-			int v = (int) ((int) &v - (int) (__brkval ?: &__heap_start));
-			out.print(v);
-			out.println('B');
-			return v > 0;
-		}
-	};
-
-	struct Echo : Command {
-
-		RUNNER_COMMAND(Echo)
-
-		int8_t run(
-			InterfaceBase * scope,
-			String args[],
-			Stream & in,
-			Stream & out,
-			Stream &
-		){
-			auto s = StringStream(args + 1);
-			while (s.available())
-			{
-				out.write(s.read());
+			int8_t run(
+				InterfaceBase * scope,
+				String args[],
+				Stream & in,
+				Stream & out,
+				Stream &
+			){
+				auto s = StringStream(args + 1);
+				while (s.available())
+				{
+					out.write(s.read());
+				}
+				out.println();
+				return 0;
 			}
-			out.println();
-			return 0;
-		}
-	};
+		};
 
-	struct Cat : Command {
+		struct Cat : Command {
 
-		RUNNER_COMMAND(Cat)
+			RUNNER_COMMAND(Cat)
 
-		int8_t run(
-			InterfaceBase * scope,
-			String args[],
-			Stream & in,
-			Stream & out,
-			Stream &
-		){
-			Stream * d = &in;
-			if(args[1].length()) {
+			int8_t run(
+				InterfaceBase * scope,
+				String args[],
+				Stream & in,
+				Stream & out,
+				Stream &
+			){
+				Stream * d = &in;
+				if(args[1].length()) {
+					auto s = scope->find<Stream>(args[1]);
+					if(s == nullptr){
+						return -1;
+					}
+					d = s->ref();
+				}
+				while (d->available())
+				{
+					out.write(d->read());
+				}
+				return 0;
+			}
+		};
+
+		struct Info : Command {
+
+			RUNNER_COMMAND(Info)
+
+			int8_t run(
+				InterfaceBase * scope,
+				String args[],
+				Stream & in,
+				Stream & out,
+				Stream & err
+			){
+				if(args[1].length()) {
+					auto ptr = scope->find(args[1]);
+					if(ptr == nullptr){
+						return -1;
+					}
+					out.println(ptr->info());
+				} else {
+					for(auto ptr = scope->modules; ptr != nullptr; ptr = ptr->next){
+						out.println(ptr->info());
+					}
+				}
+				return 0;
+			}
+		};
+
+		struct Status : Command {
+
+			RUNNER_COMMAND(Status)
+
+			int8_t run(
+				InterfaceBase * scope,
+				String args[],
+				Stream & in,
+				Stream & out,
+				Stream & err
+			){
+				// if args[1] print status of the command else print status of all cmd
+				return 0;
+			}
+		};
+
+		struct Flush : Command {
+
+			RUNNER_COMMAND(Flush)
+
+			int8_t run(
+				InterfaceBase * scope,
+				String args[],
+				Stream & in,
+				Stream & out,
+				Stream & err
+			){
 				auto s = scope->find<Stream>(args[1]);
 				if(s == nullptr){
+					err.println(args[1] + " not found");
 					return -1;
 				}
-				d = s->ref();
+				s->ref()->flush();
+				return 0;
 			}
-			while (d->available())
-			{
-				out.write(d->read());
-			}
-			return 0;
-		}
-	};
-
-	struct Info : Command {
-
-		RUNNER_COMMAND(Info)
-
-		int8_t run(
-			InterfaceBase * scope,
-			String args[],
-			Stream & in,
-			Stream & out,
-			Stream & err
-		){
-			if(args[1].length()) {
-				auto ptr = scope->find(args[1]);
-				if(ptr == nullptr){
-					return -1;
-				}
-				out.println(ptr->info());
-			} else {
-				for(auto ptr = scope->modules; ptr != nullptr; ptr = ptr->next){
-					out.println(ptr->info());
-				}
-			}
-			return 0;
-		}
-	};
-
-	struct Status : Command {
-
-		RUNNER_COMMAND(Status)
-
-		int8_t run(
-			InterfaceBase * scope,
-			String args[],
-			Stream & in,
-			Stream & out,
-			Stream & err
-		){
-			// if args[1] print status of the command else print status of all cmd
-			return 0;
-		}
-	};
-
-	struct Flush : Command {
-
-		RUNNER_COMMAND(Flush)
-
-		int8_t run(
-			InterfaceBase * scope,
-			String args[],
-			Stream & in,
-			Stream & out,
-			Stream & err
-		){
-			auto s = scope->find<Stream>(args[1]);
-			if(s == nullptr){
-				err.println(args[1] + " not found");
-				return -1;
-			}
-			s->ref()->flush();
-			return 0;
-		}
-	};
+		};
+	}
 }
